39721439dfb6970b8fdc6a570a35f7145ce3b531
Zbignev Antonovic <zb.antonovic@gmail.com>
2018-04-16 13:39:44 +0300

    trying to solve refresh problem on heroku

--- server.js
+++ server.js
@@ -7,3 +7,16 @@ app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+var path = require('path');
+var router = express.Router();
+ 
+// serve angular front end files from root path
+router.use('/', express.static('app', { redirect: false }));
+ 
+// rewrite virtual urls to angular app to enable refreshing of internal pages
+router.get('*', function (req, res, next) {
+    res.sendFile(path.resolve('app/index.html'));
+});
+ 
+module.exports = router;
