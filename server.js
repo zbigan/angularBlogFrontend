0a54eb56b8bf4b9fe89c26ef7a1c5efedd813785
Zbignev Antonovic <zb.antonovic@gmail.com>
2018-04-18 11:37:39 +0300

    minor changes

--- server.js
+++ server.js
@@ -2,18 +2,16 @@
 const express = require('express');
 const app = express();
 
+const path = require('path');
+// ...
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+// app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 
-// var path = require('path');
- 
-// // serve angular front end files from root path
-// app.use('/', express.static('app', { redirect: false }));
- 
-// // rewrite virtual urls to angular app to enable refreshing of internal pages
-// app.get('*', function (req, res, next) {
-//     res.sendFile(path.resolve('app/index.html'));
-// });
