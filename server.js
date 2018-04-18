1e535e6d230443096b2224f0bdebea0255da6e87
Zbignev Antonovic <zb.antonovic@gmail.com>
2018-04-18 11:53:57 +0300

    minor changes

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 // ...
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/src/index.html'));
-});
+// app.get('/*', function(req, res) {
+//   res.sendFile(path.join(__dirname + '/src/index.html'));
+// });
 // Serve only the static files form the dist directory
-// app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
